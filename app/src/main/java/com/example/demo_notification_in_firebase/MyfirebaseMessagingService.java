85726673b229d33c1ee062624a078322418f7870
minhnd <lammjnhphong4560@gmail.com>
2024-06-06 20:58:14 +0700

    first commit

---
+++ app/src/main/java/com/example/demo_notification_in_firebase/MyfirebaseMessagingService.java
@@ -0,0 +1,34 @@
+package com.example.demo_notification_in_firebase;
+
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyfirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+
+
+        getFirebaseMessage(message.getNotification().getTitle(), message.getNotification().getBody());
+
+    }
+
+    private void getFirebaseMessage(String title, String body) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "notify")
+                .setSmallIcon(R.drawable.ic_bell)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(102, builder.build());
+    }
+}
